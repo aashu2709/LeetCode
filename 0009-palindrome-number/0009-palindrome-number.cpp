5a99017c0a04fc52f06b7a18dc271d80e0a77e49
Ashutosh Pal <aashu1902ap@gmail.com>
2023-05-19 20:11:17 +0530

    Time: 15 ms (66.69%), Space: 6.1 MB (16.15%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+         if(x<0)
+         return false;
+         if(x != 0 && x%10==0)
+         return false;
+         int orignal_number=x;
+        
+        long int reversed_number=0;
+        
+        int remainder;
+        
+        while(x!=0){
+            remainder=x%10;
+            reversed_number = reversed_number*10+remainder;
+            x/=10;
+        }
+        if(reversed_number==orignal_number){
+            return true;
+        }
+        else{
+            
+            return false;
+            
+        }
+        
+    }
+};
