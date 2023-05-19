2fe687547acb3f07e98cbe56a4b5771d7d6f4aa4
Ashutosh Pal <aashu1902ap@gmail.com>
2023-05-19 20:13:03 +0530

    Time: 28 ms (12.95%), Space: 5.8 MB (85.53%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -14,6 +14,7 @@ public:
         while(x!=0){
             remainder=x%10;
             reversed_number = reversed_number*10+remainder;
+            
             x/=10;
         }
         if(reversed_number==orignal_number){
