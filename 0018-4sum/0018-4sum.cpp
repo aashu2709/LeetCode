cd5e1077ad81f8c7bac4e6796e6d40442049db1f
Ashutosh Pal <aashu1902ap@gmail.com>
2023-05-26 19:24:05 +0530

    Time: 73 ms (71.18%), Space: 13.1 MB (94.87%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -14,22 +14,28 @@ public:
             {
                 if(j != (i+1) && nums[j] == nums[j-1]) continue;
                 int k = j+1;
+                
                 int l = n-1;
 
                 while(k < l)
                 {
                     long long sum = nums[i];
                     sum += nums[j];
+                    
                     sum += nums[k];
+                    
                     sum += nums[l];
 
                     if(sum == target)
                     {
                         vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        
                         ans.push_back(temp);
                         
                         k++;
+                        
                         l--;
+                        
                         while(k < l && nums[k] == nums[k-1]) k++;
                         while(k < l && nums[l] == nums[l+1]) l--;
 
