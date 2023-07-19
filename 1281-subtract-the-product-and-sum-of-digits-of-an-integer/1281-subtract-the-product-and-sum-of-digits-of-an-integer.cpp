67bb34bbd4ba17b8cdc7fc0ef7f7aa68189fe241
Ashutosh Pal <aashu1902ap@gmail.com>
2023-07-19 19:43:45 +0530

    Time: 0 ms (100.00%), Space: 6 MB (42.16%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -15,8 +15,7 @@ public:
             sum=sum+digit; 
             
             
-              n=n/10;   
-            
+              n=n/10;               
         }            
         int ans=product-sum;    
         return ans; 
