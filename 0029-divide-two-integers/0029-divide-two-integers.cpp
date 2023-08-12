e2bed8e349cec6dab6c94038240d0a29c6711036
Ashutosh Pal <aashu1902ap@gmail.com>
2023-08-13 01:07:09 +0530

    Time: 0 ms (100.00%), Space: 6 MB (14.08%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
+        
         int result;
 		if (dividend >= INT_MAX && divisor == 1)	result = INT_MAX;
 		else if(dividend <= INT_MIN && divisor == 1) result = INT_MIN;
