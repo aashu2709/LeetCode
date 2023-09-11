0ffece3a90bceeb9737c1f35d20e4b0c79a4af9a
Ashutosh Pal <aashu1902ap@gmail.com>
2023-09-11 23:51:54 +0530

    Time: 30 ms (52.03%), Space: 89.2 MB (80.37%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -37,6 +37,7 @@ public:
         if ((m + n) % 2 == 0) {
             
             for (int i = 0; i < (m + n) / 2 - 1; ++i) {
+                
                 int _ = getMin(nums1, nums2);
             }
             return (double)(getMin(nums1, nums2) + getMin(nums1, nums2)) / 2;
