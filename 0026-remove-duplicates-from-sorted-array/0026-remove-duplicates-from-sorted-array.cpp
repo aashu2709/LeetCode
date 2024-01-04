bc4a4d5f098be70d9d3d120c14de25669aabdb6b
Ashutosh Pal <aashu1902ap@gmail.com>
2024-01-04 18:26:18 +0530

    Time: 0 ms (100.00%), Space: 18.7 MB (54.47%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -8,9 +8,7 @@ public:
         for(int i = 1; i < nums.size(); i++){
             
             
-            if(nums[i] != nums[i - 1]){
-                
-                
+            if(nums[i] != nums[i - 1]){            
                 nums[j] = nums[i];
                 
                 
