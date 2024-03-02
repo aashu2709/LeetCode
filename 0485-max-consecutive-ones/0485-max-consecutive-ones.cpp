d6d30ec6b1d5f4b1d1d429efef5075e04c6948b2
Ashutosh Pal <aashu1902ap@gmail.com>
2024-03-02 20:05:06 +0530

    Time: 20 ms (90.41%), Space: 38.5 MB (50.48%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -4,6 +4,7 @@ public:
          int ctn = 0;
         int ans = INT_MIN;
         for(int i=0; i<nums.size(); i++){
+            
             if(nums[i]!=1){
                 ans = max(ans, ctn);
                 ctn = 0;
