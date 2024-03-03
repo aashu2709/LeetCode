13c79af4d8a48cd007c6353a0efcd75306bdbf46
Ashutosh Pal <aashu1902ap@gmail.com>
2024-03-03 19:31:13 +0530

    Time: 495 ms (50.07%), Space: 218.5 MB (57.73%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.cpp
+++ 0493-reverse-pairs/0493-reverse-pairs.cpp
@@ -12,6 +12,7 @@ void merge(vector<int>&arr,int low,int mid,int high,int &cnt){
         }
          
         cnt+=c;
+         
         
           while(j<=high and arr[j]<=arr[i])v.push_back(arr[j++]);
           v.push_back(arr[i++]);
